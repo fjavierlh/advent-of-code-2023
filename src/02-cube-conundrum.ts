63683f9f83090c20583a736045535cb626a5e269
Javier López <fjavierlh@gmail.com>
2023-12-07 00:47:32 +0100

    refactor: add types and extract some methods

--- src/02-cube-conundrum.ts
+++ src/02-cube-conundrum.ts
@@ -27,45 +27,72 @@ import { extractInput } from "./util/extract-input";
  *
  * Determine which games would have been possible if the bag had been loaded with only 12 red cubes, 13 green cubes, and 14 blue cubes. What is the sum of the IDs of those games?
  * Your puzzle answer was 2771.
+ *
+ * --- Part Two ---
+ * The Elf says they've stopped producing snow because they aren't getting any water! He isn't sure why the water stopped; however, he can show you how to get to the water source to check it out for yourself. It's just up ahead!
+ *
+ * As you continue your walk, the Elf poses a second question: in each game you played, what is the fewest number of cubes of each color that could have been in the bag to make the game possible?
+ *
+ * Again consider the example games from earlier:
+ *
+ * Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+ * Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+ * Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+ * Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+ * Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+ * In game 1, the game could have been played with as few as 4 red, 2 green, and 6 blue cubes. If any color had even one fewer cube, the game would have been impossible.
+ * Game 2 could have been played with a minimum of 1 red, 3 green, and 4 blue cubes.
+ * Game 3 must have been played with at least 20 red, 13 green, and 6 blue cubes.
+ * Game 4 required at least 14 red, 3 green, and 15 blue cubes.
+ * Game 5 needed no fewer than 6 red, 3 green, and 2 blue cubes in the bag.
+ * The power of a set of cubes is equal to the numbers of red, green, and blue cubes multiplied together. The power of the minimum set of cubes in game 1 is 48. In games 2-5 it was 12, 1560, 630, and 36, respectively. Adding up these five powers produces the sum 2286.
+ *
+ * For each game, find the minimum set of cubes that must have been present. What is the sum of the power of these sets?
  */
+type BoxColor = "red" | "green" | "blue";
+type Play = Array<[BoxColor, number]>;
+type Game = [number, Array<Play>];
+
+const maxBoxesLimitByColor: Record<BoxColor, number> = {
+  red: 12,
+  green: 13,
+  blue: 14,
+};
+
+function recordToGame(record: string): Game {
+  const [rawId, rawPlays] = record.split(":");
+
+  const id = +rawId.split(" ")[1];
+  const plays = rawPlays
+    .split(";")
+    .map((plays: string) => plays.split(","))
+    .map((plays: string[]) =>
+      plays
+        .map((play) => play.split(" "))
+        .map(([_, quantity, color]): [BoxColor, number] => [
+          color as BoxColor,
+          +quantity,
+        ])
+    );
+
+  return [id, plays];
+}
+
+function isPossibleGame([_, plays]: Game): boolean {
+  return !plays.some((play) =>
+    play.some(([color, quantity]) => quantity > maxBoxesLimitByColor[color])
+  );
+}
 
 export function cubeConundrum(records: string[]): number {
-  const maxBoxesByColor: Record<string, number> = {
-    red: 12,
-    green: 13,
-    blue: 14,
-  };
-
-  function isPossibleGame([_id, plays]: [string, Array<[string, number][]>]) {
-    return !plays.some((play) =>
-      play.some(([color, quantity]) => quantity > maxBoxesByColor[color])
-    );
-  }
-
-  const games = records
-    .map((record: string): string[] => record.split(":"))
-    .map(([rawId, rawPlays]): [string, Array<[string, number][]>] => {
-      const id = rawId.split(" ")[1];
-      const plays = rawPlays
-        .split(";")
-        .map((plays: string) => plays.split(","))
-        .map((plays: string[]) =>
-          plays
-            .map((play) => play.split(" "))
-            .map(([_, quantity, color]): [string, number] => [color, +quantity])
-        );
-
-      return [id, plays];
-    });
-
-  const possibleGamesIDs: number[] = games
+  return records
+    .map(recordToGame)
     .filter(isPossibleGame)
-    .map(([id]) => +id);
-
-  return possibleGamesIDs.reduce((sum, num) => sum + num, 0);
+    .map(([id]) => id)
+    .reduce((sum, num) => sum + num, 0);
 }
 
 export const input = extractInput("02-input.txt");
 const result = cubeConundrum(input);
 
-console.log("02 *\tCube Conundrum \n\tResult =>", result);
+console.log("\n02 *\tCube Conundrum \n\tResult =>", result);
